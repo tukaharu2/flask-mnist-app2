dc3bb095387246a936723167f07f74b1a57102ef
tukaharu2 <tukaharu1@gmail.com>
2022-12-27 21:04:37 +0900

    first commit

--- main.py
+++ main.py
@@ -69,13 +69,13 @@ def upload_file():
                 return pred_mask[0]
 
             def display(display_list):
-                plt.figure(figsize=(15, 15))
+                plt.figure(figsize=(20, 20))
 
-                title = ['Input Image', 'True Mask', 'Predicted Mask']
+                
 
                 for i in range(len(display_list)):
                     plt.subplot(1, len(display_list), i+1)
-                    plt.title(title[i])
+                    
                     plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
                     plt.axis('off')
                 plt.savefig("./static/mask_img/mask.jpeg")
